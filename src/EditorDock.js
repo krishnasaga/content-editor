8d1ed41fb521ebb1d15ee90918e9833d1dc67f3c
Krishna Sagar R <ragas.sagar.474@gmail.com>
2021-07-01 18:42:40 +0100

    Initial commit

---
+++ src/EditorDock.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Box, Grid, Input, Text, Button, Slider, Radio } from "theme-ui";
+import { Portal } from "react-portal";
+import Editor from "./Editor";
+
+export const EditorDock = ({
+  open,
+  texts = [1, 2],
+  media = [1],
+  knobs = [1, 2],
+  switches = [1, 2, 3, 4]
+}) => {
+  if (!open) {
+    return null;
+  }
+  return (
+    <Portal>
+      <Box
+        sx={{
+          width: "45%",
+          height: "100%",
+          position: "fixed",
+          right: 0,
+          top: 0,
+          boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)"
+        }}
+      >
+        <Editor />
+      </Box>
+    </Portal>
+  );
+};
